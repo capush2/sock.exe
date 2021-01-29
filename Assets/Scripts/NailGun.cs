d0b4c9794640913bf7a0c4d7d664b58ec7c81740
Jimmy Bell <jimmy.bell@polymtl.ca>
2021-01-28 19:24:30 -0500

    NailGun uwu

---
+++ Assets/Scripts/NailGun.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NailGun : MonoBehaviour//, IWeaponTool
+{
+    [SerializeField]
+    GameObject nailPrefab = null;
+
+    [SerializeField]
+    Vector3 relativeNailStartCoords = new Vector3(0.29f, -0.01f, 1.04f);
+
+    private bool isEquiped;
+
+    void Awake()
+    {
+        isEquiped = false;
+    }
+
+    void FixedUpdate()
+    {
+        if(isEquiped && Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("HasClicked");
+            Instantiate(nailPrefab, relativeNailStartCoords, Quaternion.identity, transform);
+            gameObject.SetActive(false);
+            isEquiped = false;
+        }
+    }
+    /*
+    public GameObject Equip()
+    {
+
+    }
+
+    public GameObject Use()
+    {
+
+    }*/
+}
--- Assets/Scripts/NailVelocity.cs
+++ Assets/Scripts/NailVelocity.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class NailVelocity : MonoBehaviour
 {
-    
-    void Start()
+    [SerializeField]
+    float impulseForce = 150;
+    void Awake()
     {
-        GetComponent<Rigidbody>().AddForce(Vector3.forward*15, ForceMode.Impulse);
+        GetComponent<Rigidbody>().AddForce(Vector3.forward*impulseForce*Time.deltaTime, ForceMode.Impulse);
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        var v = transform.position + GetComponent<Rigidbody>().velocity;
+        transform.LookAt(v);
+        Debug.Log(v);
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, (transform.position + GetComponent<Rigidbody>().velocity) * 5);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
     }
 }
