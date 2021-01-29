49977e8533aff38ad3dcfbae1e4e557f0bbcd6c4
Jimmy Bell <jimmy.bell@polymtl.ca>
2021-01-28 19:25:47 -0500

    Iweapon beginns

--- Assets/Scripts/NailGun.cs
+++ Assets/Scripts/NailGun.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NailGun : MonoBehaviour//, IWeaponTool
+public class NailGun : MonoBehaviour, IWeaponTool
 {
     [SerializeField]
     GameObject nailPrefab = null;
