f55a1741531f31a958507dac02409e3546b21ad0
Jimmy Bell <jimmy.bell@polymtl.ca>
2021-01-29 16:19:39 -0500

    Canvas and UI work

---
+++ Assets/InventoryUIManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUIManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    public GameObject[] inventoryDisplay = new GameObject[3];
+    public Sprite[] inventorySprites = new Sprite[3];
+    public void AddToInventory()
+    {
+
+    }
+}
