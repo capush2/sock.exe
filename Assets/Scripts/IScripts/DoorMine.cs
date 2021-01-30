22c04d684d4656ed49a2a4de4df5ef8777e68225
capush2 <olivierga@hotmail.com>
2021-01-30 01:09:16 -0500

    Mine

--- Assets/Scripts/IScripts/DoorMine.cs
+++ Assets/Scripts/IScripts/DoorMine.cs
@@ -5,15 +5,51 @@ using UnityEngine;
 
 public class DoorMine : WeaponTool
 {
+    [SerializeField] private ParticleSystem explosion;
+    private float triggerAngle = 30f;
+
+    private bool isPlaced = false;
+    private Quaternion trigger = Quaternion.identity;
     public override bool Use(RaycastHit hit)
     {
+        
         if (hit.transform != null && hit.transform.tag == "Door")
         {
-            transform.parent = hit.transform;
+            //EDIT: ca fait 2 min que je l'ai codé pi jme rappel pu comment ca marche
+            //Ne pas changer
+            Transform target = hit.transform;
+            GameObject intruder = new GameObject();
+            intruder.transform.parent = target;
             gameObject.SetActive(true);
+            transform.parent = intruder.transform;
+            transform.position = hit.point;
+
+
+
+            transform.eulerAngles = (hit.normal + Vector3.up) * 90;
+            transform.parent = target;
+            Destroy(intruder);
+            //Fin du bloc de no edit
+
+            trigger = transform.rotation;
+            isPlaced = true;
+            
+
+
             return true;
         }
         return false;
     }
 
+    private void FixedUpdate()
+    {
+        if(isPlaced && Quaternion.Angle(trigger,transform.rotation) > triggerAngle)
+        {
+            ParticleSystem exp = Instantiate(explosion,transform.position,Quaternion.identity);
+            exp.Play();
+            Debug.Log("Boom");
+            Destroy(gameObject);
+            Destroy(exp, 10);
+        }
+    }
 }
