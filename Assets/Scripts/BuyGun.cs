18e08ed901a651c9ed8e4ec3be3e9920caa7082e
Christophe St-Georges <cstg23@hotmail.ca>
2021-01-31 14:15:01 -0500

    Done the Weapon store page

---
+++ Assets/Scripts/BuyGun.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuyGun : MonoBehaviour
+{
+    [SerializeField] GameObject gun;
+    [SerializeField] GameManager gm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(AddGun);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void AddGun()
+    {
+        gun = Instantiate(gun);
+        gun.transform.Translate(16f, 0.5f, 94f);
+        gm.SendPlayerMessage("Gun bought!");
+    }
+}
---
+++ Assets/Scripts/GetMoney.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetMoney : MonoBehaviour
+{
+    Text label;
+    // Start is called before the first frame update
+    void Start()
+    {
+        label = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string compare = GameManager.money.ToString() + " $";
+        if (compare != label.text)
+            label.text = compare;
+    }
+}
