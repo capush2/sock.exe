d8862411f45506327515123d6d709cd078b4b29b
capush2 <olivierga@hotmail.com>
2021-01-31 12:38:40 -0500

    suce moi la wesh

--- Assets/Scripts/BreedingGroup.cs
+++ Assets/Scripts/BreedingGroup.cs
@@ -20,17 +20,17 @@ public class BreedingGroup : MonoBehaviour
             current.SetActive(true);
             
             GameObject daddy = transform.Find("InventaireSlots").gameObject;
-            current.transform.SetParent(daddy.transform);
+            current.transform.SetParent(daddy.transform,true);
             current.transform.localScale = new Vector3(1, 1, 1);
 
             float width = daddy.transform.GetComponent<RectTransform>().rect.width / 2;
             float height = daddy.transform.GetComponent<RectTransform>().rect.height / 2;
             float side = current.transform.GetComponent<RectTransform>().rect.width / 2;
-            float hGap = (width - 3 * side) / 3;
-            float vGap = (height - 3 * side) / 3;
+            float hGap = (width - 3 * side);
+            float vGap = (height - 3 * side);
 
-            current.transform.position = daddy.transform.position;
-            current.transform.position += new Vector3((i % 3 + 1) * hGap - side / 2, -(Mathf.FloorToInt(i / 3) + 1/2f) * vGap - side / 2 + side/4, 0);
+            current.transform.localPosition = Vector3.zero;
+            current.transform.localPosition += new Vector3((i % 3 + 1) * hGap - side / 2, -(Mathf.FloorToInt(i / 3) + 1 / 2f) * vGap - side / 2 + side / 4, 0);
         }
     }
     public void Effacer()
